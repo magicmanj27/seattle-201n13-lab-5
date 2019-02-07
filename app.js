64d569ff3758959b2b6a3077d4461f6d9835d660
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-02-07 10:03:36 -0800

    Solved Problem 4

--- app.js
+++ app.js
@@ -140,20 +140,36 @@ testSumArray(testArray);
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is
-the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were
+Write a function called multiplyArray() that takes an array of numbers as its
+argument and returns an array whose first element is
+the product of those numbers, and the second element is a string that EXACTLY
+follows this example and uses the values that were
 input into the function:
 "The numbers 2,3,4 have a product of 24."
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function.
+To do multiplication, use your multiply() function that you've already created.
+You're going to have to be resourceful to figure out how to do this. This function
+should handle an array containing three elements. However, you may continue to use the 
++ operator for string concatenation.
+Test this function by hand in the console to get it working, and when you think it is finished,
+uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  if (multArr instanceof Array) {
+    var myProd = 1;
+    for (var i = 0; i < multArr.length; i++) {
+      myProd *= multiply(multArr[i])[0];
+    }
+  }
+  var message = 'The numbers ' + multArr + ' have a product of ' + myProd + '.';
+  return ([myProd, message]);
 }
 
+
+
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
